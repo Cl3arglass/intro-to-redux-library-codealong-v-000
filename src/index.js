b0f324a374c6120a8c6ef10ddf00191251cff88b
Holden Altaffer <haltaffer@gmail.com>
2019-04-20 00:21:06 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -10,3 +10,12 @@ import './index.css';
 const store = createStore(shoppingListItemReducer); /* code change */
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <Provider store={store}>
+        {' '}
+        /* code change */
+        <App />
+    </Provider> /* code change */,
+    document.getElementById('root')
+);
